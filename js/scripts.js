fd97629bcbfe77211626ab96fcdeb0528955c2de
Saoud <saoud@saoud.dev>
2021-02-06 21:23:46 -0800

    Fix form, syntax, logic, seperating business and UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,59 @@
-  $(document).ready(function() {
-    $("#langSelect").submit(function(event) {
-        event.preventDefault();
-        const q1 = $("input:form-control[name=question1]:checked").val();
-        console.log(q1)
-        const q2 = $("input:form-control[name=question2]:checked").val();
-        console.log(q2)
-        const q3 = $("input:form-control[name=question3]:checked").val();
-        console.log(q3)
-        const q4 = $("input:form-control[name=question4]:checked").val();
-        console.log(q4)
-        const q5 = $("input:form-control[name=question5]:checked").val();
-        console.log(q5)
-        //To exapnd upon this just add more logic below so all answers are accounted for.
 
-        if (q1 === "Python" && q2 === "Scripting") {
-          $("#Python").show();
-      } else if (q1 === "JavaScript" && q3 === "smile") {
-          $("#R").show(); 
-      } else if (q5 === "enjoy") {
-          $("#JavaScript").show();
-      } else if (q4 === "edm") {
-          $("#Python").show()
-      } else if (q3 === "frown") {
-          $("#R").show()
-      } 
+function chooseResult(sum) {
+    if (sum < 5) {
+      return "python"
+    } else if (sum >= 5 && sum <10) {
+      return "Javascript"
+    } else {
+      return "R"
+    }
+  }  
+  
+  $(document).ready(function() {
+    $("form#q1").submit(function(event) {
+    
+      let total=0;
+      
+      $("input:checkbox[name=language]:checked").each(function() {
+        const number = $(this).val()
+        total += parseInt(number)
+      });
+  
+      $("input:checkbox[name=accomplish]:checked").each(function() {
+        const number = $(this).val()
+        total += parseInt(number)
+      });
+  
+      $("input:checkbox[name=life]:checked").each(function() {
+        const number = $(this).val()
+        total += parseInt(number)
+      });
+  
+      $("input:checkbox[name=music]:checked").each(function() {
+        const number = $(this).val()
+        total += parseInt(number)
+      });
+
+      $("input:checkbox[name=reason]:checked").each(function() {
+        const number = $(this).val()
+        total += parseInt(number)
+      });
+
+    if (chooseResult(total) === "Python") {
+      $('#Python').show();
+      $('#R').hide();
+      $('#Javascript').hide();
+    } else if (chooseResult(total) === "Javascript") {
+      $('#Javascript').show();
+      $('#R').hide();
+      $('#Python').hide();
+    } else {
+      $('#R').show();
+      $('#Python').hide();
+      $('#Javascript').hide();
+    }
+
+    event.preventDefault();
+    }); 
   });
-});
+  
